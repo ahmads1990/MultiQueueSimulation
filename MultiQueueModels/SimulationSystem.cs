7cc57de3a9154c05e9dccc8ca17ef8f6b8213c78
DalaOsos <94072822+DalaOsos@users.noreply.github.com>
2022-10-25 23:32:16 +0200

    Update SimulationSystem.cs

--- MultiQueueModels/SimulationSystem.cs
+++ MultiQueueModels/SimulationSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections; 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,10 @@ namespace MultiQueueModels
         ///////////// OUTPUTS /////////////
         public List<SimulationCase> SimulationTable { get; set; }
         public PerformanceMeasures PerformanceMeasures { get; set; }
+        
+        Random r_s_id = new Random();
+	    Random r_s = new Random();
+
 
         public void startSimulation() { }
         public void DistributionTimeCustomer(List<int> inter_arrival_time, List<decimal> probability)
@@ -85,6 +90,177 @@ namespace MultiQueueModels
                     return list_of_distribution[i].Time;
             }
             return 0;
+        }
+        private void calculate_service_time(ref SimulationCase next_customer)
+        {
+
+            next_customer.RandomService = r_s.Next(1, 100);
+            int used_index = (next_customer.AssignedServer.ID)- 1;
+            int l = Servers[used_index].TimeDistribution.Count;
+
+            for (int i = 0; i < l; i++)
+            {
+
+                if (next_customer.RandomService >= Servers[next_customer.AssignedServer.ID - 1].TimeDistribution[i].MinRange &&
+                    next_customer.RandomService <= Servers[next_customer.AssignedServer.ID - 1].TimeDistribution[i].MaxRange)
+                {
+
+                    next_customer.ServiceTime = Servers[next_customer.AssignedServer.ID - 1].TimeDistribution[i].Time;
+                    break;
+                }
+
+            }
+
+            next_customer.EndTime = next_customer.ServiceTime + next_customer.StartTime;
+        }
+        private void check_Priority(ref SimulationCase next_customer)
+        {    
+	  List<int> Currently_available = new List<int>();
+           	 for (int i = 0; i< Servers.Count; ++i)
+            		{
+                		bool foundService = false;
+
+               		 	for (int j = SimulationTable.Count - 1; j >= 0; j--)
+               			 {
+                    			if (SimulationTable[j].AssignedServer.ID == i + 1)
+                    			{
+                        		 foundService = true;
+                        		 if (next_customer.ArrivalTime >= SimulationTable[j].EndTime)
+                               		 Currently_available.Add(i + 1); 
+                        
+                         	   	 break; 
+                    			}
+                		}
+                		if (!foundService)
+                    		Currently_available.Add(i + 1); // the server is free
+               
+            		}
+
+		 if (Currently_available.Count == 1)
+			{
+				next_customer.TimeInQueue = 0; 
+                		next_customer.StartTime = next_customer.ArrivalTime;
+             		        next_customer.AssignedServer.ID = Currently_available[0];
+				calculate_service_time(ref next_customer);
+            			Servers[next_customer.AssignedServer.ID - 1].TotalWorkingTime += next_customer.ServiceTime;
+			}
+		else if (Currently_available.Count == 0) //search for the nearset server will be available   
+            	{
+                	int min_diffrence = 2000000000;
+              		int min_ID = 1000000000; 
+                	List<int> Nearset_will_be_available = new List<int>(); 
+                	for (int k = 0; k < Servers.Count; ++k)
+                	{
+                    		for (int i = SimulationTable.Count - 1; i >= 0; i--)
+                    		{
+                        	if (SimulationTable[i].AssignedServer.ID == k + 1)
+                        	{
+                            	if (SimulationTable[i].EndTime - next_customer.ArrivalTime < min_diffrence)
+                            	{
+                                min_diffrence = SimulationTable[i].EndTime - next_customer.ArrivalTime;
+                                min_ID = SimulationTable[i].AssignedServer.ID;
+                            	}
+                            	break; 
+                        	}
+                        
+                    	}	
+                	}
+
+                Nearset_will_be_available.Add(min_ID);
+
+                for (int k = 0; k < Servers.Count; k++)
+                {
+                    for (int i = SimulationTable.Count - 1; i >= 0; i--)
+                    {
+                        if (k + 1 == SimulationTable[i].AssignedServer.ID)
+                        {
+                            if (SimulationTable[i].EndTime - next_customer.ArrivalTime == min_diffrence &&
+                                SimulationTable[i].AssignedServer.ID != min_ID)
+                            { 
+                                Nearset_will_be_available.Add(SimulationTable[i].AssignedServer.ID);
+                            }
+                            break;
+                        }
+                        
+                    }
+                }
+                
+                Nearset_will_be_available.Sort(); 
+                next_customer.TimeInQueue = min_diffrence;
+                next_customer.StartTime = next_customer.ArrivalTime + next_customer.TimeInQueue;  // get the time in the queue and the start time of the service  
+
+                if (Nearset_will_be_available.Count == 1)
+                   next_customer.AssignedServer.ID = Nearest_will_be_available[0];
+		   calculate_service_time(ref next_customer);
+            	   Servers[next_customer.AssignedServer.ID - 1].TotalWorkingTime += next_customer.ServiceTime;
+
+                else if (Nearset_will_be_available.Count > 1)
+                {
+                    Selection_methods(ref next_customer, Nearset_will_be_available);
+                }
+            }
+             
+            else if (Currently_available.Count > 1)
+            {
+                next_customer.TimeInQueue = 0; // No wait TimeInQueue 
+                next_customer.StartTime = next_customer.ArrivalTime;// start time = the arrival time 
+                Selection_methods(ref next_customer, Currently_available);
+            }
+
+        }
+private void Selection_methods(ref SimulationCase next_customer, List<int> server)
+        {
+            // priority Selcetion Method   
+                if (SelectionMethod == Enums.SelectionMethod.HighestPriority)
+                    select_HighestPriority(ref next_customer, server);
+
+                //random genertate  
+                else if (SelectionMethod == Enums.SelectionMethod.Random)
+                    select_Random(ref next_customer, server);
+
+                // utilization  
+                else if (SelectionMethod == Enums.SelectionMethod.LeastUtilization)
+                    select_LeastUtilization(ref next_customer, server);
+        }
+
+
+
+        private void select_HighestPriority(ref SimulationCase next_customer, List<int> server)
+        {
+            next_customer.AssignedServer.ID = server[0];
+            calculate_service_time(ref next_customer);
+
+            Servers[next_customer.AssignedServer.ID - 1].TotalWorkingTime += next_customer.ServiceTime;
+        }
+
+
+
+        private void select_Random(ref SimulationCase next_customer, List<int> server) //changed
+        {
+            next_customer.AssignedServer.ID = server[r_s_id.Next(0, server.Count)]; //-1
+            calculate_service_time(ref next_customer);
+            Servers[next_customer.AssignedServer.ID - 1].TotalWorkingTime += next_customer.ServiceTime;
+            
+        }
+
+
+
+        private void select_LeastUtilization(ref SimulationCase next_customer, List<int> server)
+        {
+            int min_ID = Servers[server[0] - 1].ID;
+            int min_Work = Servers[server[0] - 1].TotalWorkingTime;
+            for (int i = 1; i < server.Count; ++i)
+            {
+               
+                if (Servers[server[i] - 1].TotalWorkingTime < min_Work)
+                {
+                    min_Work = Servers[server[i] - 1].TotalWorkingTime;
+                    min_ID = Servers[server[i] - 1].ID;
+                }
+            }
+            next_customer.AssignedServer.ID = min_ID;
+            calculate_service_time(ref next_customer);
+            Servers[next_customer.AssignedServer.ID - 1].TotalWorkingTime += next_customer.ServiceTime;
         }
 
     }
